2e9f6b003468e13e590c7e3bc489106b198a090f
Joseph Sawaya <josephsawaya938@gmail.com>
2020-07-23 23:23:24 -0400

    initial ses mock commit

---
+++ lib/providers/ses/ses-provider.ts
@@ -0,0 +1,45 @@
+import { v4 as uuidv4 } from "uuid";
+type SendEmailParams = {
+  Destination: {
+    /* required */
+    BccAddresses?: string[];
+    CcAddresses?: string[];
+    ToAddresses: string[];
+  };
+  Message: {
+    /* required */
+    Body: {
+      /* required */
+      Html: {
+        Data: string /* required */;
+      };
+      Text: {
+        Data: string /* required */;
+      };
+    };
+    Subject: {
+      /* required */ Data: string /* required */;
+    };
+  };
+  Source: string /* required */;
+  ConfigurationSetName?: string;
+  ReplyToAddresses?: string[];
+  ReturnPath?: string;
+  ReturnPathArn?: string;
+  SourceArn?: string;
+  Tags?: { Name: string; Value: string }[];
+};
+export class SESLocalClient {
+  constructor() {
+    this.sentEmails = {};
+  }
+  sentEmails: { [key: string]: SendEmailParams };
+  async sendEmail(params: SendEmailParams): Promise<string> {
+    const messageId = uuidv4();
+    this.sentEmails[messageId] = params;
+    return messageId;
+  }
+  getEmail(messageId: string): SendEmailParams {
+    return this.sentEmails[messageId];
+  }
+}
