ced7f916968ba70371c78b1484e1dcc1c17e77d2
Jeremi Gendron <gendronjeremi@gmail.com>
2020-08-04 00:08:01 -0400

    v1.1.1-rc.2-patch02

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,6 @@
 export * from "./utils";
 export * from "./types";
 export * from "./lambda";
-export * from "./providers";
+// export * from "./providers";
+/** Not exporting providers cause some code is node-only */
+/** Just import from @bgpc/aws-serverless-toolkit/dist/providers */
